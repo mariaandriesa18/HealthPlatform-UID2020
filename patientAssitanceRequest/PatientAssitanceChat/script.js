36a746c66261b2b005a2f27cc3f01fb3448f6352
David <vdavidxx@yahoo.com>
2021-01-09 23:27:12 +0200

    - assitance request 2/2

---
+++ patientAssitanceRequest/PatientAssitanceChat/script.js
@@ -0,0 +1,30 @@
+const newMessage = document.getElementById('newMessage');
+const chatScreen =document.getElementById('chat-screen')
+
+
+
+function sendMessage(){
+
+    const messageVal = newMessage.value;
+
+    console.log(messageVal);
+
+    let template = `
+    <div class="chat-message">
+        <h6>Me</h6>
+        <p class="chat-content">${messageVal}</p>
+        <p class="send-data">${new Date().toLocaleString()}</p>
+    </div>
+    `;
+
+    let node=  document.createElement("div");
+    node.classList.add('chat-right');
+    node.innerHTML=template;
+
+    newMessage.value="";
+
+    chatScreen.appendChild(node);
+
+    chatScreen.scrollBy(0,10000);
+
+}
---
+++ patientAssitanceRequest/script.js
@@ -0,0 +1,3 @@
+function assistChat(){
+    window.location.href = "./PatientAssitanceChat";
+}
