fff77384448599e2a425359fd4cf2d68488b26ea
Maria Andriesa <maria18and@gmail.com>
2021-01-12 21:18:04 +0200

    added error message to patient appointments

--- scripts/patient/patientAppointments.js
+++ scripts/patient/patientAppointments.js
@@ -1,9 +1,3 @@
-const nextYear = new Date().getFullYear() + 1;
-
-const myCalender = new CalendarPicker('#myCalendarWrapper', {
-    min: new Date(),
-     max: new Date(nextYear, 10) 
-});
 
 function NewAppointment(){
     var table = document.getElementById("myTable");
@@ -14,24 +8,30 @@ function NewAppointment(){
 
     var dtRegex = new RegExp("^([0]?[1-9]|[1-2]\\d|3[0-1])-(JAN|FEB|MAR|APR|MAY|JUN|JULY|AUG|SEP|OCT|NOV|DEC)-[1-2]\\d{3}$", 'i');
     
-    alert(dtRegex.test(dateVal));
+    if( dtRegex.test(dateVal) == false){
+        document.getElementById("wrongDate").style.visibility = "visible";
+        document.getElementById("dateInput").value = " ";
+        document.getElementById("hourInput").value = " ";
+        document.getElementById("symptomsInput").value = " ";
+    }else{
+        var row = table.insertRow(1);
+
+        // Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+        var cell3 = row.insertCell(2);
+        var cell4 = row.insertCell(3);
+        var cell5 = row.insertCell(4);
+        // Add some text to the new cells:
+        cell1.innerHTML = `<tr> <td>${dateVal}</td>`;
+        cell2.innerHTML = `<td>${hourVal}</td>`;
+        cell3.innerHTML = `<td>${sympVal}</td>`;
+        cell4.innerHTML = `<td>Upcoming</td>`;
+        cell5.innerHTML = `<td><button type="button" class="btn btn-outline-dark" onclick="deleteRow(this)">CANCEL</button></button></td> </tr>`;
     
-    var row = table.insertRow(1);
-
-    // Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
-    var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
-    var cell3 = row.insertCell(2);
-    var cell4 = row.insertCell(3);
-    var cell5 = row.insertCell(4);
-    // Add some text to the new cells:
-    cell1.innerHTML = `<tr> <td>${dateVal}</td>`;
-    cell2.innerHTML = `<td>${hourVal}</td>`;
-    cell3.innerHTML = `<td>${sympVal}</td>`;
-    cell4.innerHTML = `<td>Upcoming</td>`;
-    cell5.innerHTML = `<td><button type="button" class="btn btn-outline-dark" onclick="deleteRow(this)">CANCEL</button></button></td> </tr>`;
-
-    alert(myCalender.value)
+    }
+    
+   
 }
 
 function deleteRow(r) {
